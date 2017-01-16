14ab8ead4a49623d89230caec6f6d4c7c1d91017
Stella Wiedjarnarko <stella.wiedjarnarko@gmail.com>
2017-01-16 11:56:27 +0700

    Update authkey.php

--- authkey.php
+++ authkey.php
@@ -22,11 +22,11 @@
 				        <h1 class="dx-content-title">Generate Signature & Authkey BCA</h1>
 				    </header>
                     <form method="POST" action="">
-                        Clearkey <input type="text" name="clearKey" value="" class="form-control"><br>
-                        Klikpay Code <input type="text" name="klikpaycode" value="" class="form-control"><br>
-                        Trx ID <input type="text" name="trx_id" value="" class="form-control"><br>
-                        Transaction Date <input type="datetime" name="transactiondate" value="" class="form-control"><br>
-                        Amount <input type="text" name="amount" value="" class="form-control"><br><br>
+                        Clearkey <input type="text" name="clearKey" value="" class="form-control" required><br>
+                        Klikpay Code <input type="text" name="klikpaycode" value="" class="form-control" required><br>
+                        Trx ID <input type="text" name="trx_id" value="" class="form-control" required><br>
+                        Transaction Date <input type="datetime" name="transactiondate" value="" class="form-control" required><br>
+                        Amount <input type="text" name="amount" value="" class="form-control" required><br><br>
 
                         <button type="submit" name="submitForm" value="submit" class="btn btn-primary">Generate</button>
                         <button type="submit" name="submitForm" value="code" class="btn btn-primary">Code</button>
