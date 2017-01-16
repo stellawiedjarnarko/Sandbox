19808221a62826b04bbc1f53d985832111eeaae9
stellawiedjarnarko <stella.wiedjarnarko@gmail.com>
2017-01-16 11:39:30 +0700

    Update authkey.php

--- authkey.php
+++ authkey.php
@@ -1,47 +1,38 @@
-<?php 
-    include "sidemenu.php";
-?>
+<body class="dx-apiref dx-apis">
+    
+    <?php 
+        include "head.php";
+    ?>
 
-<!DOCTYPE html>
-
-<html lang="en">
-    <head>
-    </head>
-
-    <body>
-        <div id="wrapper">
-            <div id="page-wrapper">
-                <div class="container-fluid">
-			<h1 align="center">Generate Signature & Authkey BCA</h1>
-	                <hr style="width: 100px">
+    <main class="dx-main">
+        <div class="dx-flex dx-core">
+            <div class="dx-main-nav-slug"></div>
+		    <?php
+                include "sidemenu";
+            ?>
+                <article class="dx-content dx-flex-block ">
+                    <?php
+                        echo "<br>";
+                        if(!empty($response))
+                        {
+                            echo "<div class='alert alert-success'><pre>".$response."</pre></div>";
+                        }
+                    ?>
+				    <header>
+				        <h1 class="dx-content-title">Generate Signature & Authkey BCA</h1>
+				    </header>
                     <form method="POST" action="">
                         Clearkey <input type="text" name="clearKey" value="" class="form-control"><br>
                         Klikpay Code <input type="text" name="klikpaycode" value="" class="form-control"><br>
                         Trx ID <input type="text" name="trx_id" value="" class="form-control"><br>
                         Transaction Date <input type="datetime" name="transactiondate" value="" class="form-control"><br>
-                        Amount <input type="text" name="amount" value="" class="form-control"><br>
-                        
-                        <button type="submit" name="submitForm" value="submit" class="btn btn-primary">Submit</button>
+                        Amount <input type="text" name="amount" value="" class="form-control"><br><br>
+
+                        <button type="submit" name="submitForm" value="submit" class="btn btn-primary">Generate</button>
                         <button type="submit" name="submitForm" value="code" class="btn btn-primary">Code</button>
                     </form>
-
-                    <?php
-                        echo "<br>";
-                        if(!empty($response))
-                        {
-                            echo "<div class='alert alert-success' style='text-transform:uppercase;'><pre>".$response."</pre></div>";
-                        }
-                        else if(!empty($response2))
-                        {
-                            echo "<div class='alert alert-info' style='text-transform:uppercase;'><pre><b>CODE:</b><br/>".$response2."</pre></div>";
-                        }
-                    ?>
-
-                </div>
-            </div>
-        </div>
-
-        <script src="<?php echo base_url('assets/js/jquery.js'); ?>"></script>
-        <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
+				</article>
+			</div>
+        </main>
+        <script id="PayPalAnalytics" src="<?php echo base_url('assets/js/pa.js');?>"></script>
     </body>
-</html>
